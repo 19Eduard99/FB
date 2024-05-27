d6cceed9a66e447801ff134fb4eba010ee23a960
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-05-27 23:25:12 +0300

    adaptive is done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-const swiper = new Swiper('.swiper', {
+let swiper = new Swiper('.swiper', {
     loop: false,
     slidesPerView: 4,
     spaceBetween: 30,
     speed: 1000,
-
-    // autoplay: {
-    //     delay: 2000,
-    //     disableOnInteraction: false,
-    // },
     pagination: {
         el: '.swiper-pagination',
         clickable: false,
@@ -23,14 +18,37 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
     breakpoints: {
+        100: {
+            slidesPerView: 1,
+            pagination: {
+                type: 'bullets',
+            },
+        },
+
         320: {
             slidesPerView: 1,
+            pagination: {
+                type: 'bullets',
+            },
         },
         480: {
             slidesPerView: 2,
+            pagination: {
+                type: 'bullets',
+            },
         },
         768: {
+            
             slidesPerView: 3,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+                renderFraction: function (currentClass, totalClass) {
+                    return '<span class="' + currentClass + '"></span>' +
+                           ' из ' +
+                           '<span class="' + totalClass + '"></span>';
+                }
+            }
         },
         1024: {
             slidesPerView: 4,
